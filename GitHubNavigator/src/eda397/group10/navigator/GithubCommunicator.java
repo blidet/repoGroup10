842e7d67b6532d7aa7fc5ee9797b38d013cc6535
unknown <Oscar@Oscar-LAP.(none)>
2014-04-08 14:04:03 +0200

    Worked a bit on the connection architecture.

--- GitHubNavigator/src/eda397/group10/navigator/GithubCommunicator.java
+++ GitHubNavigator/src/eda397/group10/navigator/GithubCommunicator.java
@@ -1,21 +1,49 @@
 package eda397.group10.navigator;
 
+import java.io.IOException;
+
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.HTTP;
+
 import android.os.AsyncTask;
 
 public class GithubCommunicator extends AsyncTask<String, Void, String> {
+	
+	private final Header currentHeader;
+	
+	public GithubCommunicator(String[] url, Header header){
+		currentHeader = header;
+		this.execute(url);
+	}
 
 	@Override
 	protected String doInBackground(String... url) {
-		// TODO Auto-generated method stub
-		return null;
+		HttpClient client = new DefaultHttpClient();
+		HttpGet request = new HttpGet(url[0]);
+		request.addHeader(currentHeader);
+		
+		HttpResponse response = null;
+		try {
+			response = client.execute(request);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		//return response.getStatusLine().getStatusCode();
+		return response.toString();
 	}
 
 	@Override
 	protected void onPostExecute(String result) {
 		// TODO Auto-generated method stub
 		super.onPostExecute(result);
+		//requester.handleResponse(result);
 	}
-	
-	
 
 }
