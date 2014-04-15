5991c26f4ec4271c20aee570044bd71178e5ac81
Sundin <sundin666@gmail.com>
2014-04-15 17:14:59 +0200

    Right now the app just gives the user a notification once a minute

--- GitHubNavigator/src/eda397/group10/navigator/MainActivity.java
+++ GitHubNavigator/src/eda397/group10/navigator/MainActivity.java
@@ -3,9 +3,12 @@ package eda397.group10.navigator;
 import eda397.group10.communication.Constants;
 import eda397.group10.notifications.NotificationService;
 import android.support.v7.app.ActionBarActivity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -61,8 +64,9 @@ public class MainActivity extends ActionBarActivity {
      * Starts an alarm that will initiate the poll for updates in github, and then repeat itself.
      */
     private void startAlarm() {
-    /*	SharedPreferences prefs = getSharedPreferences(Constants.SETTINGS_PREFERENCES,0);
-        int minutes = prefs.getInt("interval", 1);
+    	SharedPreferences prefs = getSharedPreferences(Constants.SETTINGS_PREFERENCES,0);
+        //int minutes = prefs.getInt("interval", 1);
+    	int minutes =1;
         AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
         Intent i = new Intent(this, NotificationService.class);
         PendingIntent pi = PendingIntent.getService(this, 0, i, 0);
@@ -72,10 +76,11 @@ public class MainActivity extends ActionBarActivity {
             am.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
                 SystemClock.elapsedRealtime() + minutes*60*1000,
                 minutes*60*1000, pi);
-        }*/
+
+            Log.println(Log.ASSERT, "alarm", "started");
+        }
         
-        Log.println(Log.ASSERT, "alarm", "started");
-        startService(new Intent(this, NotificationService.class));
+       // startService(new Intent(this, NotificationService.class));
     }
 
 }
--- GitHubNavigator/src/eda397/group10/notifications/NotificationService.java
+++ GitHubNavigator/src/eda397/group10/notifications/NotificationService.java
@@ -9,10 +9,17 @@ import org.apache.http.protocol.HTTP;
 import eda397.group10.communication.Constants;
 import eda397.group10.communication.GithubRequest;
 import eda397.group10.navigator.MainActivity;
+import eda397.group10.navigator.ProjectPageActivity;
+import eda397.group10.navigator.R;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.app.TaskStackBuilder;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 /**
@@ -30,7 +37,7 @@ public class NotificationService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
     	Log.println(Log.ASSERT, "get notifications", ".........");
         handleIntent(intent);
-        return START_STICKY;
+        return START_NOT_STICKY;
     }
     
     private void handleIntent(Intent intent) {
@@ -66,11 +73,43 @@ public class NotificationService extends Service {
 			Integer statusCode = result.getStatusLine().getStatusCode();
 			Log.println(Log.ASSERT, "get notifications", "status code: "+statusCode+" NOTIFICATION");
 			
+			createNotification();
 			stopSelf();
 		}
 	}
     
     public void onDestroy() {
         super.onDestroy();
+    }
+    
+    private void createNotification() {
+    	NotificationCompat.Builder mBuilder =
+    	        new NotificationCompat.Builder(this)
+    	        .setSmallIcon(R.drawable.news72)
+    	        .setContentTitle("My notification")
+    	        .setContentText("Hello World!");
+    	// Creates an explicit intent for an Activity in your app
+    	Intent resultIntent = new Intent(this, MainActivity.class);
+
+    	// The stack builder object will contain an artificial back stack for the
+    	// started Activity.
+    	// This ensures that navigating backward from the Activity leads out of
+    	// your application to the Home screen.
+    	TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+    	// Adds the back stack for the Intent (but not the Intent itself)
+    	//stackBuilder.addParentStack(MainActivity.class);
+    	// Adds the Intent that starts the Activity to the top of the stack
+    	stackBuilder.addNextIntent(resultIntent);
+    	PendingIntent resultPendingIntent =
+    	        stackBuilder.getPendingIntent(
+    	            0,
+    	            PendingIntent.FLAG_UPDATE_CURRENT
+    	        );
+    	mBuilder.setContentIntent(resultPendingIntent);
+    	NotificationManager mNotificationManager =
+    	    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+    	// mId allows you to update the notification later on.
+    	int mId = 1;
+    	mNotificationManager.notify(mId, mBuilder.build());
     }
 }
