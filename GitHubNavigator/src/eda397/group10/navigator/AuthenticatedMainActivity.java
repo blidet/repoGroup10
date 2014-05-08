9a933231727a1c79b00a8e83d30eca7af3a097f0
Azin <Azin@129.16.232.252>
2014-05-08 11:35:26 +0200

    change confirmation message

--- GitHubNavigator/src/eda397/group10/navigator/AuthenticatedMainActivity.java
+++ GitHubNavigator/src/eda397/group10/navigator/AuthenticatedMainActivity.java
@@ -382,7 +382,7 @@ public class AuthenticatedMainActivity extends Activity{
 					
 			android.app.AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
 			alertDialog.setTitle("Logout...");
-			alertDialog.setMessage("Are you sure?");
+			alertDialog.setMessage("Are you sure you want to logout?");
 			alertDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
 				
 				public void onClick(DialogInterface dialog, int which) {
@@ -592,7 +592,7 @@ public class AuthenticatedMainActivity extends Activity{
 				}
 			} catch (JSONException e) {
 				// TODO Auto-generated catch block
-				e.printStackTrace(); 
+				e.printStackTrace();
 			}
 
 			/*
